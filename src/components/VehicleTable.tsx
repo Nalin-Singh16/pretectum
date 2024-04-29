e74f4de4777c3b5b074b6372ef176dc7d36f0040
Nalin-Singh16 <nalin.singh16@gmail.com>
2024-04-29 05:49:25 +0530

    fixed bugs

--- src/components/VehicleTable.tsx
+++ src/components/VehicleTable.tsx
@@ -36,11 +36,17 @@ const VehicleTable = () => {
 
     const columns: ColumnsType<VehicleData> = [
         { title: 'Name', dataIndex: 'Name', key: 'Name' },
-        { title: 'Model', dataIndex: 'Model', key: 'Model', filteredValue: filterInfo.Model ? [filterInfo.Model] : null, filters: modelFilters, filterSearch: true, onFilter: (value: boolean | Key, record: VehicleData) => record.Model.includes(String(value)) },
-        { title: 'Type', dataIndex: 'Type', key: 'Type', filteredValue: filterInfo.Type ? [filterInfo.Type] : null, filters: typeFilters, filterSearch: true, onFilter: (value: boolean | Key, record: VehicleData) => record.Type.includes(String(value)) },
-        { title: 'Manufacturer', dataIndex: 'Manufacturer', key: 'Manufacturer', filteredValue: filterInfo.Manufacturer ? [filterInfo.Manufacturer] : null, filters: manufacturerFilters, filterSearch: true, onFilter: (value: boolean | Key, record: VehicleData) => record.Manufacturer.includes(String(value)) },
-        { title: 'Manufacturing Date', dataIndex: 'Manufacturing Date', key: 'Manufacturing Date', sorter: true },
-        { title: 'Seating', dataIndex: 'Seating', key: 'Seating', filteredValue: filterInfo.Seating ? [filterInfo.Seating] : null, filters: seatingFilters, filterSearch: true, onFilter: (value: boolean | Key, record: VehicleData) => record.Seating === value }
+        { title: 'Model', dataIndex: 'Model', key: 'Model', filteredValue: Array.isArray(filterInfo.Model) ? filterInfo.Model : [], filters: modelFilters, filterSearch: true, onFilter: (value: boolean | Key, record: VehicleData) => record.Model.includes(String(value)) },
+        { title: 'Type', dataIndex: 'Type', key: 'Type', filteredValue: Array.isArray(filterInfo.Type) ? filterInfo.Type : [], filters: typeFilters, onFilter: (value: boolean | Key, record: VehicleData) => record.Type.includes(String(value)) },
+        { title: 'Manufacturer', dataIndex: 'Manufacturer', key: 'Manufacturer', filteredValue: Array.isArray(filterInfo.Manufacturer) ? filterInfo.Manufacturer : [], filters: manufacturerFilters, filterSearch: true, onFilter: (value: boolean | Key, record: VehicleData) => record.Manufacturer.includes(String(value)) },
+        {
+            title: 'Manufacturing Date', dataIndex: 'Manufacturing Date', key: 'Manufacturing Date', sorter: (a, b) => {
+                const dateA = new Date(a['Manufacturing Date']).getTime();
+                const dateB = new Date(b['Manufacturing Date']).getTime();
+                return dateA - dateB;
+            }
+        },
+        { title: 'Seating', dataIndex: 'Seating', key: 'Seating', filteredValue: Array.isArray(filterInfo.Seating) ? filterInfo.Seating : [], filters: seatingFilters, filterSearch: true, onFilter: (value: boolean | Key, record: VehicleData) => record.Seating === value }
     ];
 
     return (
